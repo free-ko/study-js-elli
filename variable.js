6904fb18aceb96f6e655cb6bd7a4183199dec68c
kyw <[duddnr654@naver.com]>
2020-06-25 15:51:08 +0900

    2. Variable_complete

--- variable.js
+++ variable.js
@@ -51,3 +51,57 @@ const maxNumber = 5;
 // 4. Variable types
 // primitive, single item: number, string, boolean, null, undefined, symbol
 // object, box container
+// function, first-class function(변수 안에 함수를 넣을 수 있음)
+
+const count = 17; // integer
+const size = 17.1; // decimal number
+
+// number - special number values : infinity, -infinity, NaN
+const infinity = 1 / 0;
+const negativeInfinity = -1 / 0;
+const NaN = 'not a number' / 2;
+// Dom요소를 post해서 JS에 가져올 때 혹은 다양한 계산을 해야 할 때 숫자인지 확인 안 하고 사용 하면 안 됨
+
+// bigInt (fairly new, don't use it yet)
+const bigInt = 12312312312312312312312312312312312313123123; // over (-2*53 ~ 2*53)
+Number.MAX_SAFE_INTEGER; 
+
+// string 
+const char = 'C';
+const brendan = 'brendan';
+const greeting = 'hello' + brendan;
+const helloBob = `h1 ${brendan}`; // template literals (string)
+
+// boolean
+// false: 0, null, undefined, NaNk, ''
+// true: any other value
+const canRead = true;
+const test = 3 < 1; // false
+
+// null
+let nothing = null; // 비어있다
+
+// undefined
+let x; // 비어있는지 값이 있는지 모를 때
+
+// symbol, create unique identifiers for objects
+const symbol1 = Symbol('id');
+const symbol2 = Symbol('id');
+symol1 === symbol2 // false
+// 같은 심볼로 만들어도 고유한 심볼로 만들어짐 그래서 나중에 무언가 구별할 때 유용함
+
+const gSymbol1 = Symbol.for('id');
+const gSymbol2 = Symbol.for('id');
+gSymbol1 === gSymbol2 // True
+
+// symbol은 바로 출력하면 에러가 뜸 그래서 symbol1.description이라고 해서 출력해야 됨
+
+// object, real-life object, data structure
+const freeko = { name: 'freeko', age: 29 };
+freeko.age = 21; // 변경가능
+
+// 5. Dynamic typing: dynamically typed language
+let text = 'hello';
+text = 1;
+text = '7' + 5; // type을 변경해서 '75'로 변경 
+text = '8' / '2'; // number로 인식해서 자동적으로 계산함
